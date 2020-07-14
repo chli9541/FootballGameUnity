d90ed8df59cb93c20fe04508dfc75135680980db
Jonas_QD <richmondjonas@gmail.com>
2020-07-14 16:21:51 -0700

    Added position specific growth

--- Winter Project A/Assets/Scripts/DBRelatedScripts/ButtonPlayerEvaluation.cs
+++ Winter Project A/Assets/Scripts/DBRelatedScripts/ButtonPlayerEvaluation.cs
@@ -15,16 +15,7 @@ public class ButtonPlayerEvaluation : MonoBehaviour
         myBtn.onClick.AddListener(delegate
         {
             Player player = new Player();
-            player.shooting = 90;
-            player.pace = 89;
-            player.anticipation = 80;
-            player.tackling = 60;
-            player.header = 78;
-            player.shortPass = 78;
-            player.longPass = 75;
-            player.strength = 65;
-            player.stamina = 70;
-            player.agility = 80;
+            GrowthCalculation.ResetPlayer(player);
 
             //float MainGrowth = (float)0.08;
             //float SecondaryGrowth = (float)0.04;
@@ -40,38 +31,14 @@ public class ButtonPlayerEvaluation : MonoBehaviour
             float WF = PlayerEvaluater.EvaluateWF(player);
             float WM = PlayerEvaluater.EvaluateWM(player);
             //orginal player evaluation
-            Debug.Log("AMold" + AM);
-            Debug.Log("CBold" + CB);
-            Debug.Log("CFold" + CF);
-            Debug.Log("CMold" + CM);
-            Debug.Log("DMold" + DM);
-            Debug.Log("FBold" + FB);
-            Debug.Log("STold" + ST);
-            Debug.Log("WFold" + WF);
-            Debug.Log("WMold" + WM);
-
-
-
 
             for (int i = 1; i < 61; i++)
             {
-                float MainGrowth18 = RandomFloat(0.05,0.09);
-                float SecondaryGrowth18 = RandomFloat(0.03, 0.05);
-                float LittleGrowth18 = RandomFloat(0.01, 0.03);
-                
-
-                player.shooting = player.shooting+SecondaryGrowth18;
-                player.pace += MainGrowth18;
-                player.anticipation += LittleGrowth18;
-                player.tackling += SecondaryGrowth18;
-                player.header += SecondaryGrowth18;
-                player.shortPass += SecondaryGrowth18;
-                player.longPass += SecondaryGrowth18;
-                player.strength += MainGrowth18;
-                player.stamina += MainGrowth18;
-                player.agility += MainGrowth18;
+                GrowthCalculation.ChangePlayerUnder18(player);
+                GrowthCalculation.ChangePlayerWM(player);
             }
 
+           
 
             float AMnow = PlayerEvaluater.EvaluateAM(player);
             float CBnow = PlayerEvaluater.EvaluateCB(player);
@@ -83,38 +50,24 @@ public class ButtonPlayerEvaluation : MonoBehaviour
             float WFnow = PlayerEvaluater.EvaluateWF(player);
             float WMnow = PlayerEvaluater.EvaluateWM(player);
 
-            Debug.Log("AM18 one year after compared with orginal::" + (AMnow-AM));
-            Debug.Log("CB18 one year after compared with orginal::" + (CBnow-CB));
-            Debug.Log("CF18 one year after compared with orginal::" + (CFnow-CF));
-            Debug.Log("CM18 one year after compared with orginal::" + (CMnow-CM));
-            Debug.Log("DM18 one year after compared with orginal::" + (DMnow-DM));
-            Debug.Log("FB18 one year after compared with orginal::" + (FBnow-FB));
-            Debug.Log("ST18 one year after compared with orginal::" + (STnow-ST));
-            Debug.Log("WF18 one year after compared with orginal::" + (WFnow-WF));
-            Debug.Log("WM18 one year after compared with orginal::" + (WMnow-WM));
+            //Debug.Log("AM18 one year after compared with orginal:" + (AMnow-AM));
+            //Debug.Log("CB18 one year after compared with orginal:" + (CBnow-CB));
+            //Debug.Log("CF18 one year after compared with orginal:" + (CFnow-CF));
+            //Debug.Log("CM18 one year after compared with orginal:" + (CMnow-CM));
+            //Debug.Log("DM18 one year after compared with orginal:" + (DMnow-DM));
+            //Debug.Log("FB18 one year after compared with orginal:" + (FBnow-FB));
+            //Debug.Log("ST18 one year after compared with orginal:" + (STnow-ST));
+            //Debug.Log("WF18 one year after compared with orginal:" + (WFnow-WF));
+            Debug.Log("WM18 one year after compared with orginal:" + (WMnow-WM));
 
-            player.shooting = 10;
-            player.pace = 10;
-            player.anticipation = 10;
-            player.tackling = 10;
-            player.header = 10;
-            player.shortPass = 10;
-            player.longPass = 10;
-            player.strength = 10;
-            player.stamina = 10;
-            player.agility = 10;
+            GrowthCalculation.ResetPlayer(player);
 
             for (int i = 1; i < 61; i++)
             {
-                float MainGrowth2932 = RandomFloat(-0.05, -0.02);
-                float LittleGrowth2932 = RandomFloat(0.005, 0.015);
-
-                player.pace += MainGrowth2932;
-                player.anticipation += LittleGrowth2932;
-                player.strength += MainGrowth2932;
-                player.stamina += MainGrowth2932;
-                player.agility += MainGrowth2932;
+                GrowthCalculation.ChangePlayer29to32(player);
+                GrowthCalculation.ChangePlayerWM(player);
             }
+
 
             float AM29 = PlayerEvaluater.EvaluateAM(player);
             float CB29 = PlayerEvaluater.EvaluateCB(player);
@@ -126,14 +79,17 @@ public class ButtonPlayerEvaluation : MonoBehaviour
             float WF29 = PlayerEvaluater.EvaluateWF(player);
             float WM29 = PlayerEvaluater.EvaluateWM(player);
 
-            Debug.Log("AM29 one year after compared with orginal:" + (AM29 - 10));
-            Debug.Log("CB29 one year after compared with orginal:" + (CB29 - 10));
-            Debug.Log("CF29 one year after compared with orginal:" + (CF29 - 10));
-            Debug.Log("CM29 one year after compared with orginal:" + (CM29 - 10));
-            Debug.Log("DM29 one year after compared with orginal:" + (DM29 - 10));
-            Debug.Log("FB29 one year after compared with orginal:" + (FB29 - 10));
-            Debug.Log("ST29 one year after compared with orginal:" + (ST29 - 10));
-            Debug.Log("WF29 one year after compared with orginal:" + (WF29 - 10));
+
+
+
+            //Debug.Log("AM29 one year after compared with orginal:" + (AM29 - 10));
+            //Debug.Log("CB29 one year after compared with orginal:" + (CB29 - 10));
+            //Debug.Log("CF29 one year after compared with orginal:" + (CF29 - 10));
+            //Debug.Log("CM29 one year after compared with orginal:" + (CM29 - 10));
+            //Debug.Log("DM29 one year after compared with orginal:" + (DM29 - 10));
+            //Debug.Log("FB29 one year after compared with orginal:" + (FB29 - 10));
+            //Debug.Log("ST29 one year after compared with orginal:" + (ST29 - 10));
+            //Debug.Log("WF29 one year after compared with orginal:" + (WF29 - 10));
             Debug.Log("WM29 one year after compared with orginal:" + (WM29 - 10));
 
 
---
+++ Winter Project A/Assets/Scripts/DBRelatedScripts/GrowthCalculation.cs
@@ -0,0 +1,261 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowthCalculation : MonoBehaviour
+{
+    public static float RandomFloat(double minValue, double maxValue)
+    {
+        int RandomIndex = Random.Range(1, 5); //5 is arbitrarily chosen
+
+        return (float)(RandomIndex * (maxValue - minValue) / 5 + minValue);
+
+    }
+    public static void ChangePlayerUnder18(Player player) {
+        float MainGrowth18 = RandomFloat(0.04, 0.07);
+        float SecondaryGrowth18 = RandomFloat(0.02, 0.04);
+        float LittleGrowth18 = RandomFloat(0.01, 0.03);
+
+        player.shooting = player.shooting + SecondaryGrowth18;
+        player.pace += MainGrowth18;
+        player.anticipation += LittleGrowth18;
+        player.tackling += SecondaryGrowth18;
+        player.header += SecondaryGrowth18;
+        player.shortPass += SecondaryGrowth18;
+        player.longPass += SecondaryGrowth18;
+        player.strength += MainGrowth18;
+        player.stamina += MainGrowth18;
+        player.agility += MainGrowth18;
+    }
+    public static void ChangePlayer29to32(Player player)
+    {
+        float MainDecrease2932 = RandomFloat(-0.1, -0.06);
+        float LittleGrowth2932 = RandomFloat(0.005, 0.015);
+
+        player.pace += MainDecrease2932;
+        player.anticipation += LittleGrowth2932;
+        player.strength += MainDecrease2932;
+        player.stamina += MainDecrease2932;
+        player.agility += MainDecrease2932;
+    }
+    /// <summary>
+    /// will add more increase to attributes that weigh more than 10%
+    /// </summary>
+    /// <param name="player"></param>
+    //shooting = 15%;
+    //pace = 12%
+    //anticipation = 11%;
+    //tackling = 3%;
+    //header = 9%;
+    //shortpass = 12%;
+    //longpass = 12%;
+    //strength = 7%;
+    //stamina = 8%;
+    //agility = 10%;
+    public static void ChangePlayerAM(Player player) {
+
+        float PositionGrowth = RandomFloat(0.015, 0.025);
+
+
+        player.shooting += PositionGrowth;
+        player.pace += PositionGrowth;
+        player.anticipation += PositionGrowth;
+        player.shortPass += PositionGrowth;
+        player.longPass += PositionGrowth;
+        
+
+    }
+    //shooting = 2%;
+    //pace = 7%
+    //anticipation = 21%;
+    //tackling = 20%;
+    //header = 10%;
+    //shortpass = 5%;
+    //longpass = 5%;
+    //strength = 15%;
+    //stamina = 10%;
+    //agility = 5%;
+    public static void ChangePlayerCB(Player player)
+    {
+
+        float PositionGrowth = RandomFloat(0.01, 0.02);
+
+
+        player.anticipation += PositionGrowth;
+        player.tackling += PositionGrowth;
+        player.strength += PositionGrowth;
+        player.stamina += PositionGrowth;
+        player.header += PositionGrowth;
+
+
+    }
+
+    //shooting = 20%;
+    //pace = 9%
+    //anticipation = 15%;
+    //tackling = 0%;
+    //header = 16%;
+    //shortpass = 5%;
+    //longpass = 5%;
+    //strength = 15%;
+    //stamina = 10%;
+    //agility = 5%;
+    public static void ChangePlayerCF(Player player)
+    {
+
+        float PositionGrowth = RandomFloat(0.02, 0.03);
+
+
+        player.shooting += PositionGrowth;
+        player.anticipation += PositionGrowth;
+        player.strength += PositionGrowth;
+
+
+        
+    }
+
+    public static void ChangePlayerCM(Player player)
+    {
+
+        float PositionGrowth = RandomFloat(0.0075, 0.015);
+        player.shooting += PositionGrowth;
+        player.pace += PositionGrowth;
+        player.anticipation += PositionGrowth;
+        player.tackling += PositionGrowth;
+        player.header += PositionGrowth;
+        player.shortPass += PositionGrowth;
+        player.longPass += PositionGrowth;
+        player.strength += PositionGrowth;
+        player.stamina += PositionGrowth;
+        player.agility += PositionGrowth;
+    }
+
+    //shooting = 5%;
+    //pace = 8%
+    //anticipation = 18%;
+    //tackling = 15%;
+    //header = 10%;
+    //shortpass = 6%;
+    //longpass = 6%;
+    //strength = 10%;
+    //stamina = 14%;
+    //agility = 8%;
+    public static void ChangePlayerDM(Player player)
+    {
+
+        float PositionGrowth = RandomFloat(0.01, 0.02);
+        player.shooting += PositionGrowth;
+        
+        player.anticipation += PositionGrowth;
+        player.tackling += PositionGrowth;
+        player.header += PositionGrowth;
+
+        player.strength += PositionGrowth;
+        player.stamina += PositionGrowth;
+        
+    }
+    //shooting = 4%;
+    //pace = 12%
+    //anticipation = 11%;
+    //tackling = 18%;
+    //header = 7%;
+    //shortpass = 8%;
+    //longpass = 8%;
+    //strength = 9%;
+    //stamina = 10%;
+    //agility = 13%;
+    public static void ChangePlayerFB(Player player)
+    {
+
+        float PositionGrowth = RandomFloat(0.015, 0.025);
+        
+        player.anticipation += PositionGrowth;
+        player.tackling += PositionGrowth;
+        player.stamina += PositionGrowth;
+        player.agility += PositionGrowth;
+    }
+
+    public static void ChangePlayerST(Player player)
+    {
+        //shooting = 16%;
+        //pace = 13%
+        //anticipation = 11%;
+        //tackling = 1%;
+        //header = 13%;
+        //shortpass = 9%;
+        //longpass = 7%;
+        //strength = 10%;
+        //stamina = 10%;
+        //agility = 10%;
+
+        float PositionGrowth = RandomFloat(0.002, 0.03);
+        player.shooting += PositionGrowth;
+        player.pace += PositionGrowth;
+        player.anticipation += PositionGrowth;
+        player.header += PositionGrowth;
+        player.strength += PositionGrowth;
+        player.stamina += PositionGrowth;
+        player.agility += PositionGrowth;
+    }
+    //shooting = 10%;
+    //pace = 20%
+    //anticipation = 7%;
+    //tackling = 1%;
+    //header = 7%;
+    //shortpass = 8%;
+    //longpass = 8%;
+    //strength = 7%;
+    //stamina = 12%;
+    //agility = 20%;
+    public static void ChangePlayerWF(Player player)
+    {
+
+        float PositionGrowth = RandomFloat(0.01, 0.03);
+        player.shooting += PositionGrowth;
+        player.pace += PositionGrowth;
+        player.pace += (float)0.01;
+        player.stamina += PositionGrowth;
+
+        player.agility += PositionGrowth;
+        player.agility += (float)0.01;
+    }
+    //shooting = 10%;
+    //pace = 15%
+    //anticipation = 10%;
+    //tackling = 7%;
+    //header = 7%;
+    //shortpass = 10%;
+    //longpass = 10%;
+    //strength = 7%;
+    //stamina = 12%;
+    //agility = 12%;
+    public static void ChangePlayerWM(Player player)
+    {
+        float PositionGrowth = RandomFloat(0.01, 0.03);
+        player.pace += PositionGrowth;
+        player.anticipation += PositionGrowth;
+        player.shortPass += PositionGrowth;
+        player.longPass += PositionGrowth;
+        player.stamina += PositionGrowth;
+        player.agility += PositionGrowth;
+        
+    }
+
+    /// <summary>
+    /// reset a player's ability to make future calculation consistent
+    /// </summary>
+    /// <param name="player"></param>
+    public static void ResetPlayer(Player player) {
+        player.shooting = 10;
+        player.pace = 10;
+        player.anticipation = 10;
+        player.tackling = 10;
+        player.header = 10;
+        player.shortPass = 10;
+        player.longPass = 10;
+        player.strength = 10;
+        player.stamina = 10;
+        player.agility = 10;
+    }
+
+}
